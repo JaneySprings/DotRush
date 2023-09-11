2878a781520e46296b0ede16e1bacb2d251ea0df
Nikita Romanov <nikita.romanov@devexpress.com>
2023-09-11 10:09:33 +0400

    context menu fixes

--- src/commands.ts
+++ src/commands.ts
@@ -25,8 +25,11 @@ export class CommandsController {
         }));
     }
 
-    private static async selectProjectFile(workspaceFolder: vscode.Uri): Promise<vscode.Uri | undefined> {
-        const files = await vscode.workspace.findFiles(new vscode.RelativePattern(workspaceFolder, '**/*.csproj'));
+    private static async selectProjectFile(targetUri: vscode.Uri): Promise<vscode.Uri | undefined> {
+        if (targetUri.fsPath.endsWith(".csproj"))
+            return targetUri;
+
+        const files = await vscode.workspace.findFiles(new vscode.RelativePattern(targetUri, '**/*.csproj'));
         if (files.length === 0) {
             vscode.window.showErrorMessage(res.messaeNoProjectFileFound);
             return undefined;
@@ -34,8 +37,8 @@ export class CommandsController {
         if (files.length === 1)
             return files[0];
 
-        const selected = await vscode.window.showQuickPick(files.map(it => path.basename(it.fsPath)), { placeHolder: res.messageSelectProjectFile });
-        return selected !== undefined ? vscode.Uri.file(selected) : undefined;
+        const selectedItem = await vscode.window.showQuickPick(files.map(it => new ProjectItem(it)), { placeHolder: res.messageSelectProjectFile });
+        return selectedItem?.uri;
     }
 }
 
@@ -45,8 +48,18 @@ class DotNetTaskProvider {
             return undefined;
         
         const command = `dotnet ${target} "${projectFile.fsPath}"`;
-        return new vscode.Task({ type: `${res.extensionId}.task` }, 
+        return new vscode.Task({ type: `${res.extensionId}.task` },
             vscode.TaskScope.Workspace, target, res.extensionId, new vscode.ShellExecution(command), res.microsoftProblemMatcherId
         );
+    }
+}
+
+class ProjectItem implements vscode.QuickPickItem {
+    label: string;
+    uri: vscode.Uri;
+
+    constructor(projectPath: vscode.Uri) {
+        this.label = path.basename(projectPath.fsPath);
+        this.uri = projectPath;
     }
 }
