16a0c5c2465be85353a4ac7db520b72a71966295
Nikita Romanov <nikita.romanov@devexpress.com>
2023-11-25 12:36:38 +0400

    Ask user before loading multiple projects in the multiple folders

--- src/vscode/client.ts
+++ src/vscode/client.ts
@@ -1,6 +1,6 @@
 import { LanguageClient, ServerOptions } from "vscode-languageclient/node";
-import * as vscode from "vscode";
 import * as res from './resources';
+import * as vscode from 'vscode';
 import * as path from 'path';
 
 
@@ -33,19 +33,30 @@ export class ClientController {
     public static async activate(context: vscode.ExtensionContext) {
         if (ClientController.client !== undefined && ClientController.client.isRunning())
             return;
+
+        if (vscode.workspace.workspaceFolders?.length === 1 && vscode.workspace.workspaceFile === undefined) {
+            const csprojFiles = await vscode.workspace.findFiles(path.join('**', '*.csproj'));
+            if (csprojFiles != undefined && csprojFiles.length > 1) {
+                const message = res.messageMultipleProjectFilesFound.replace('{0}', csprojFiles.length.toString());
+                const result = await vscode.window.showWarningMessage(message, res.messageLoadAll);
+                if (result !== res.messageLoadAll)
+                    return;
+            }
+        }
+        
         ClientController.initialize(context);
         ClientController.client.start();
 
         context.subscriptions.push(vscode.workspace.onDidSaveTextDocument(async ev => {
             if (!ev.fileName.endsWith('.csproj'))
                 return;
-    
             const message = res.messageProjectChanged.replace('{0}', path.basename(ev.fileName));
             const result = await vscode.window.showWarningMessage(message, res.messageReload);
             if (result !== undefined)
                 vscode.commands.executeCommand(res.commandIdReloadWindow);
         }));
     }
+
     public static stop() {
         if (ClientController.client !== undefined && ClientController.client.isRunning())
             ClientController.client.stop();
--- src/vscode/resources.ts
+++ src/vscode/resources.ts
@@ -14,9 +14,9 @@ export const taskCommandRestoreId = "dotrush.restore";
 export const taskCommandCleanId = "dotrush.clean";
 
 export const messageOmniSharpAlreadyInstalled = "The C# extension is already installed. Please uninstall or disable it to avoid conflicts.";
-export const messageRuntimeNotFound = "Could not find a compatible .NET runtime installed on your machine. Make sure you have .NET 6.0 or a newer version.";
-export const messageEmbeddedRuntimeNotFound = "The .NET version installed on your machine is not compatible with the current version of the DotRush. Install the latest DotRush version.";
 export const messaeNoProjectFileFound = "No project files found in the current workspace folder.";
+export const messageMultipleProjectFilesFound = "Found {0} project files in the current workspace folder. Do you want to load all projects?";
 export const messageProjectChanged = "Project {0} has changed. Please reload the IDE to apply all changes.";
 export const messageSelectProjectFile = "Select a project file";
 export const messageReload = "Reload";
+export const messageLoadAll = "Load All";
