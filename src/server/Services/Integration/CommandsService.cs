1b07beac96847690358e7c9c0610ed4a20391883
Nikita Romanov <nikita.romanov@devexpress.com>
2023-12-20 23:30:55 +0400

    Update integration service

--- src/server/Services/Integration/CommandsService.cs
+++ src/server/Services/Integration/CommandsService.cs
@@ -7,51 +7,38 @@ namespace DotRush.Server.Services;
 public class CommandsService {
     private readonly WorkspaceService workspaceService;
     private readonly ResolveTypeRequest typeResolveRequest;
-
-    private TcpListener? commandListener;
+    private readonly TcpListener commandListener;
 
     public CommandsService(WorkspaceService workspaceService) {
         this.workspaceService = workspaceService;
-        this.typeResolveRequest = new ResolveTypeRequest(workspaceService);
+        typeResolveRequest = new ResolveTypeRequest(workspaceService);
+        commandListener = new TcpListener(IPAddress.Loopback, 0);
     }
 
-
     public void ResolveCommand(string targetPath) {
-        if (!targetPath.IsSupportedCommand())
-            return;
-
         var content = File.ReadAllText(targetPath);
         if (!string.IsNullOrEmpty(content))
             return;
 
-        var port = EnsureListenerInitialized();
+        if (!commandListener.Server.IsBound) 
+            commandListener.Start();
+
+        var port = ((IPEndPoint)commandListener.LocalEndpoint).Port;
+        StartListening();
         File.WriteAllText(targetPath, port.ToString());
     }
     public void ResolveCancellation() {
-        if (commandListener == null)
-            return;
-        commandListener.Stop();
-        commandListener.Dispose();
-        commandListener = null;
+        commandListener?.Stop();
     }
     
-    private int EnsureListenerInitialized() {
-        if (commandListener != null) 
-            return ((IPEndPoint)commandListener.LocalEndpoint).Port;
-            
-        commandListener = new TcpListener(IPAddress.Loopback, 0);
-        commandListener.Start();
-        StartListening();
-        return ((IPEndPoint)commandListener.LocalEndpoint).Port;
-    }
     private async void StartListening() {
         await ServerExtensions.SafeHandlerAsync(async () => {
-            while (true) {
-                using var client = await commandListener!.AcceptTcpClientAsync();
-                using var stream = client.GetStream();
-                using var reader = new StreamReader(stream);
-                using var writer = new StreamWriter(stream) { AutoFlush = true };
-
+            using var client = await commandListener!.AcceptTcpClientAsync();
+            using var stream = client.GetStream();
+            using var reader = new StreamReader(stream);
+            using var writer = new StreamWriter(stream) { AutoFlush = true };
+    
+            while (client.Connected) {
                 var command = await reader.ReadLineAsync();
                 var parameters = await reader.ReadLineAsync();
                 var result = string.Empty;
@@ -63,7 +50,7 @@ public class CommandsService {
 
                 if (command.Equals("ResolveType", StringComparison.OrdinalIgnoreCase))
                     result = await typeResolveRequest.HandleAsync(parameters.Split('|'));
-
+                
                 await writer.WriteLineAsync(result ?? string.Empty);
             }
         });
