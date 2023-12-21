f947a2037564d7d44148594ed4c66c1bee502cbe
Nikita Romanov <nikita.romanov@devexpress.com>
2023-12-21 20:41:45 +0400

    Fix incorrect references handler implementation

--- src/server/Handlers/Navigation/ReferencesHandler.cs
+++ src/server/Handlers/Navigation/ReferencesHandler.cs
@@ -8,10 +8,10 @@ using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 namespace DotRush.Server.Handlers;
 
 public class ReferencesHandler : ReferencesHandlerBase {
-    private WorkspaceService solutionService;
+    private WorkspaceService workspaceService;
 
     public ReferencesHandler(WorkspaceService solutionService) {
-        this.solutionService = solutionService;
+        this.workspaceService = solutionService;
     }
 
     protected override ReferenceRegistrationOptions CreateRegistrationOptions(ReferenceCapability capability, ClientCapabilities clientCapabilities) {
@@ -19,29 +19,34 @@ public class ReferencesHandler : ReferencesHandlerBase {
     }
 
     public override async Task<LocationContainer?> Handle(ReferenceParams request, CancellationToken cancellationToken) {
-        var documentId = this.solutionService.Solution?.GetDocumentIdsWithFilePath(request.TextDocument.Uri.GetFileSystemPath()).FirstOrDefault();
-        var document = this.solutionService.Solution?.GetDocument(documentId);
-        if (document == null)
+        var documentIds = workspaceService.Solution?.GetDocumentIdsWithFilePath(request.TextDocument.Uri.GetFileSystemPath());
+        if (documentIds == null || workspaceService.Solution == null)
             return null;
-
-        var sourceText = await document.GetTextAsync(cancellationToken);
-        var symbol = await SymbolFinder.FindSymbolAtPositionAsync(document, request.Position.ToOffset(sourceText), cancellationToken);
-        if (symbol == null || this.solutionService.Solution == null) 
-            return null;
-
-        var referenceSymbols = await SymbolFinder.FindReferencesAsync(symbol, this.solutionService.Solution, cancellationToken);
-        var referenceLocations = referenceSymbols
-            .SelectMany(r => r.Locations)
-            .Where(l => File.Exists(l.Document.FilePath));
 
         var result = new LocationCollection();
-        foreach (var location in referenceLocations) {
-            var locationSourceText = await location.Document.GetTextAsync(cancellationToken);
-            var referenceLocation = location.ToLocation(locationSourceText);
-            if (referenceLocation != null)
-                result.Add(referenceLocation);
-        }
+        foreach (var documentId in documentIds) {
+            var document = this.workspaceService.Solution.GetDocument(documentId);
+            if (document == null)
+                continue;
 
+            var sourceText = await document.GetTextAsync(cancellationToken);
+            var symbol = await SymbolFinder.FindSymbolAtPositionAsync(document, request.Position.ToOffset(sourceText), cancellationToken);
+            if (symbol == null || symbol.Locations == null) 
+                continue;
+
+            var referenceSymbols = await SymbolFinder.FindReferencesAsync(symbol, workspaceService.Solution, cancellationToken);
+            var referenceLocations = referenceSymbols
+                .SelectMany(r => r.Locations)
+                .Where(l => File.Exists(l.Document.FilePath));
+
+            foreach (var location in referenceLocations) {
+                var locationSourceText = await location.Document.GetTextAsync(cancellationToken);
+                var referenceLocation = location.ToLocation(locationSourceText);
+                if (referenceLocation != null)
+                    result.Add(referenceLocation);
+            }
+        }
+    
         return result.ToLocationContainer();
     }
 }
