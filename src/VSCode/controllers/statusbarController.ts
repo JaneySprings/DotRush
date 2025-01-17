7957791a5e845139a578865b58c870016fafc004
nikita.romanov <alis228300@yandex.ru>
2025-01-17 23:16:08 +0400

    [Fix] Update startup project after changes in project files

--- src/VSCode/controllers/statusbarController.ts
+++ src/VSCode/controllers/statusbarController.ts
@@ -26,6 +26,10 @@ export class StatusBarController {
         context.subscriptions.push(vscode.window.registerFileDecorationProvider(StatusBarController.projectDecorationProvider));
         context.subscriptions.push(vscode.commands.registerCommand(res.commandIdActiveProjectPath, () => StatusBarController.activeProject?.path));
         context.subscriptions.push(vscode.commands.registerCommand(res.commandIdActiveConfiguration, () => StatusBarController.activeConfiguration));
+        context.subscriptions.push(vscode.workspace.onDidSaveTextDocument(async e => {
+            if (path.extname(e.fileName) === '.csproj' && StatusBarController.activeProject !== undefined)
+                StatusBarController.performSelectProject(StatusBarController.activeProject.path);
+        }));
         
         await vscode.commands.executeCommand('setContext', res.commandIdStatusBarEnabled, true);
         await StatusBarController.updateProjectStatusBarItem();
