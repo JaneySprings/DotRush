072bc7d2cd8cb4a3ee5675b13fe01dfe8aee6b07
JaneySprings <acce.ventura@icloud.com>
2025-02-13 22:48:00 +0400

    new handler

--- src/DotRush.Roslyn.Server/Handlers/TextDocument/TypeHierarchyHandler.cs
+++ src/DotRush.Roslyn.Server/Handlers/TextDocument/TypeHierarchyHandler.cs
@@ -1,3 +1,5 @@
+using DotRush.Roslyn.Common;
+using DotRush.Roslyn.Common.Extensions;
 using DotRush.Roslyn.Server.Extensions;
 using DotRush.Roslyn.Server.Services;
 using DotRush.Roslyn.Workspaces.Extensions;
@@ -10,69 +12,71 @@ using Microsoft.CodeAnalysis.FindSymbols;
 
 namespace DotRush.Roslyn.Server.Handlers.TextDocument;
 
-[Obsolete("In testing. May not work as expected.")]
 public class TypeHierarchyHandler : TypeHierarchyHandlerBase {
     private readonly NavigationService navigationService;
-    private Dictionary<int, INamedTypeSymbol> typeHierarchyCache;
+    private readonly Dictionary<int, ISymbol> typeHierarchyCache;
 
     public TypeHierarchyHandler(NavigationService navigationService) {
         this.navigationService = navigationService;
-        typeHierarchyCache = new Dictionary<int, INamedTypeSymbol>();
+        this.typeHierarchyCache = new Dictionary<int, ISymbol>();
     }
 
     public override void RegisterCapability(ServerCapabilities serverCapabilities, ClientCapabilities clientCapabilities) {
         serverCapabilities.TypeHierarchyProvider = true;
     }
-
-    protected override async Task<TypeHierarchyResponse?> Handle(TypeHierarchyPrepareParams typeHierarchyPrepareParams, CancellationToken cancellationToken) {
+    protected override Task<TypeHierarchyResponse?> Handle(TypeHierarchyPrepareParams typeHierarchyPrepareParams, CancellationToken cancellationToken) {
         typeHierarchyCache.Clear();
-        
-        var documentId = navigationService.Solution?.GetDocumentIdsWithFilePathV2(typeHierarchyPrepareParams.TextDocument.Uri.FileSystemPath).FirstOrDefault();
-        if (documentId == null || navigationService.Solution == null)
-            return null;
 
-        var result = new List<TypeHierarchyItem>();
-        var document = navigationService.Solution.GetDocument(documentId);
-        if (document == null)
-            return null;
+        return SafeExtensions.InvokeAsync(async () => {
+            var documentId = navigationService.Solution?.GetDocumentIdsWithFilePathV2(typeHierarchyPrepareParams.TextDocument.Uri.FileSystemPath).FirstOrDefault();
+            if (documentId == null || navigationService.Solution == null)
+                return null;
 
-        var sourceText = await document.GetTextAsync(cancellationToken);
-        var symbol = await SymbolFinder.FindSymbolAtPositionAsync(document, typeHierarchyPrepareParams.Position.ToOffset(sourceText), cancellationToken);
-        if (symbol == null || symbol is not INamedTypeSymbol namedTypeSymbol)
-            return null;
+            var result = new List<TypeHierarchyItem>();
+            var document = navigationService.Solution.GetDocument(documentId);
+            if (document == null)
+                return null;
 
-        result.Add(CreateTypeHierarchyItem(namedTypeSymbol, typeHierarchyPrepareParams.TextDocument.Uri.FileSystemPath));
-        return new TypeHierarchyResponse(result);
+            var sourceText = await document.GetTextAsync(cancellationToken).ConfigureAwait(false);
+            var symbol = await SymbolFinder.FindSymbolAtPositionAsync(document, typeHierarchyPrepareParams.Position.ToOffset(sourceText), cancellationToken).ConfigureAwait(false);
+            if (symbol == null || symbol is not ITypeSymbol typeSymbol)
+                return null;
+            
+            result.Add(CreateTypeHierarchyItem(typeSymbol, typeHierarchyPrepareParams.TextDocument.Uri.FileSystemPath));
+            return new TypeHierarchyResponse(result);
+        });
     }
-
     protected override Task<TypeHierarchyResponse?> Handle(TypeHierarchySupertypesParams typeHierarchySupertypesParams, CancellationToken cancellationToken) {
         if (typeHierarchySupertypesParams.Item.Data?.Value == null)
             return Task.FromResult<TypeHierarchyResponse?>(null);
 
+        var result = new List<TypeHierarchyItem>();
         var symbol = typeHierarchyCache.GetValueOrDefault((int)typeHierarchySupertypesParams.Item.Data.Value);
-        if (symbol == null || symbol.BaseType == null)
+        if (symbol == null || symbol is not ITypeSymbol typeSymbol)
             return Task.FromResult<TypeHierarchyResponse?>(null);
 
-        var result = new List<TypeHierarchyItem>();
-        result.Add(CreateTypeHierarchyItem(symbol.BaseType, typeHierarchySupertypesParams.Item.Uri.FileSystemPath));
+        if (typeSymbol.BaseType != null)
+            result.Add(CreateTypeHierarchyItem(typeSymbol.BaseType, typeHierarchySupertypesParams.Item.Uri.FileSystemPath));
+        foreach (var iface in typeSymbol.Interfaces)
+            result.Add(CreateTypeHierarchyItem(iface, typeHierarchySupertypesParams.Item.Uri.FileSystemPath));
+
         return Task.FromResult<TypeHierarchyResponse?>(new TypeHierarchyResponse(result));
     }
-
     protected override Task<TypeHierarchyResponse?> Handle(TypeHierarchySubtypesParams typeHierarchySubtypesParams, CancellationToken cancellationToken) {
         return Task.FromResult<TypeHierarchyResponse?>(null);
     }
 
-    private TypeHierarchyItem CreateTypeHierarchyItem(INamedTypeSymbol namedTypeSymbol, string filePath) {
-        var key = namedTypeSymbol.ToDisplayString().GetHashCode();
-        typeHierarchyCache.Add(key, namedTypeSymbol);
-
+    private TypeHierarchyItem CreateTypeHierarchyItem(ISymbol symbol, string filePath) {
+        var key = symbol.ToDisplayString().GetHashCode();
+        typeHierarchyCache.TryAdd(key, symbol);
+        
         return new TypeHierarchyItem {
-            Name = namedTypeSymbol.Name,
-            Kind = namedTypeSymbol.ToSymbolKind(),
-            Detail = namedTypeSymbol.ToDisplayString(),
-            Uri = namedTypeSymbol.Locations.FirstOrDefault()?.SourceTree?.FilePath ?? filePath,
-            Range = namedTypeSymbol.Locations.FirstOrDefault()?.ToRange() ?? PositionExtensions.EmptyRange,
-            SelectionRange = namedTypeSymbol.Locations.FirstOrDefault()?.ToRange() ?? PositionExtensions.EmptyRange,
+            Name = symbol.ToDisplayString(DisplayFormat.Member),
+            Kind = symbol.ToSymbolKind(),
+            Detail = symbol.ToDisplayString(DisplayFormat.Default),
+            Uri = symbol.Locations.FirstOrDefault()?.SourceTree?.FilePath ?? filePath,
+            Range = symbol.Locations.FirstOrDefault()?.ToRange() ?? PositionExtensions.EmptyRange,
+            SelectionRange = symbol.Locations.FirstOrDefault()?.ToRange() ?? PositionExtensions.EmptyRange,
             Data = key
         };
     }
