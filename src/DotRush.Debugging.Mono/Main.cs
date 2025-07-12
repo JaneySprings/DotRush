55f83fac2003c43f80fcb251fdf84f4d4b1694d7
JaneySprings <acce.ventura@icloud.com>
2025-07-12 20:14:39 +0400

    update sdk locator

--- src/DotRush.Debugging.Mono/Main.cs
+++ src/DotRush.Debugging.Mono/Main.cs
@@ -2,6 +2,10 @@
 
 public class Program {
     private static void Main(string[] args) {
+        Console.SetError(TextWriter.Null);
+        Console.SetOut(TextWriter.Null);
+        Console.SetIn(TextReader.Null);
+
         var debugSession = new DebugSession(Console.OpenStandardInput(), Console.OpenStandardOutput());
         debugSession.Start();
     }
