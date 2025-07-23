29c50f4ef5deb3c712a794be0da6d2d927225d96
nevse <nov.eugen@gmail.com>
2025-07-23 16:07:04 +0400

    Refactor Default SymbolDisplayFormat initialization for clarity and maintainability

--- src/DotRush.Roslyn.CodeAnalysis/DisplayFormat.cs
+++ src/DotRush.Roslyn.CodeAnalysis/DisplayFormat.cs
@@ -3,10 +3,15 @@ using Microsoft.CodeAnalysis;
 namespace DotRush.Roslyn.CodeAnalysis;
 
 public static class DisplayFormat {
-    public static readonly SymbolDisplayFormat Default = SymbolDisplayFormat.FullyQualifiedFormat
-        .WithGlobalNamespaceStyle(SymbolDisplayGlobalNamespaceStyle.Omitted)
-        .WithMiscellaneousOptions(SymbolDisplayMiscellaneousOptions.None)
-        .WithMiscellaneousOptions(SymbolDisplayMiscellaneousOptions.EscapeKeywordIdentifiers);
+    public static readonly SymbolDisplayFormat Default = new SymbolDisplayFormat(
+        globalNamespaceStyle: SymbolDisplayGlobalNamespaceStyle.Omitted,
+        typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameAndContainingTypesAndNamespaces,
+        genericsOptions: SymbolDisplayGenericsOptions.IncludeTypeParameters | SymbolDisplayGenericsOptions.IncludeVariance,
+        memberOptions: SymbolDisplayMemberOptions.IncludeParameters | SymbolDisplayMemberOptions.IncludeType | SymbolDisplayMemberOptions.IncludeRef | SymbolDisplayMemberOptions.IncludeContainingType | SymbolDisplayMemberOptions.IncludeConstantValue,
+        parameterOptions: SymbolDisplayParameterOptions.IncludeExtensionThis | SymbolDisplayParameterOptions.IncludeParamsRefOut | SymbolDisplayParameterOptions.IncludeType,
+        miscellaneousOptions: SymbolDisplayMiscellaneousOptions.UseSpecialTypes,
+        delegateStyle: SymbolDisplayDelegateStyle.NameAndSignature
+    );
 
     public static readonly SymbolDisplayFormat Minimal = SymbolDisplayFormat.MinimallyQualifiedFormat
         .WithMemberOptions(SymbolDisplayMemberOptions.IncludeParameters
