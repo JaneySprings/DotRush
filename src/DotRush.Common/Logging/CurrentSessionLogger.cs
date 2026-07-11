d54e9c4b359b33ecadacd6891a38a070622b7b0e
Nikita Romanov <acce.ventura@icloud.com>
2026-07-11 12:44:44 +0400

    [Fix]: `TestPlatformFormatException: Incorrect format for TestCaseFilter` for NUnit

--- src/DotRush.Common/Logging/CurrentSessionLogger.cs
+++ src/DotRush.Common/Logging/CurrentSessionLogger.cs
@@ -4,7 +4,7 @@ namespace DotRush.Common.Logging;
 
 public static class CurrentSessionLogger {
     private static readonly Logger logger = LogManager.GetCurrentClassLogger();
-    private static bool traceDebugMessages = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("DOTRUSH_TRACE_SERVER"));
+    private static readonly bool traceDebugMessages = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("DOTRUSH_TRACE_SERVER"));
 
     static CurrentSessionLogger() {
         LogConfig.InitializeLog();
@@ -17,7 +17,11 @@ public static class CurrentSessionLogger {
         logger.Error(message);
     }
     public static void Debug(string message) {
+#if DEBUG
+        logger.Debug(message);
+#else
         if (traceDebugMessages)
             logger.Debug(message);
+#endif
     }
 }
