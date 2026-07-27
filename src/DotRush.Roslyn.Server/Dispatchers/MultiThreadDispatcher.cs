8700a0fd6a87a90ed873701b396fdf5a77f0a38d
Nikita Romanov <acce.ventura@icloud.com>
2026-07-28 01:25:05 +0400

    Update `MultiThreadDispatcher`

--- src/DotRush.Roslyn.Server/Dispatchers/MultiThreadDispatcher.cs
+++ src/DotRush.Roslyn.Server/Dispatchers/MultiThreadDispatcher.cs
@@ -1,3 +1,4 @@
+using DotRush.Common.Extensions;
 using EmmyLua.LanguageServer.Framework.Protocol.JsonRpc;
 using EmmyLua.LanguageServer.Framework.Server.Scheduler;
 
@@ -11,13 +12,32 @@ public class MultiThreadDispatcher : IScheduler {
         "textDocument/willSave",
         "textDocument/willSaveWaitUntil"
     };
+    private readonly List<Task> runningTasks = new List<Task>();
+    private readonly object runningTasksLock = new object();
 
     public void Schedule(Func<Message, Task> action, Message message) {
         if (message is MethodMessage methodMessage && syncronizedMethods.Contains(methodMessage.Method)) {
+            WaitForRunningTasks();
             action(message).Wait();
+            return;
         }
-        else {
-            _ = Task.Run(() => action(message));
-        }
+
+        var task = Task.Run(() => action(message));
+        lock (runningTasksLock)
+            runningTasks.Add(task);
+
+        task.ContinueWith(t => {
+            lock (runningTasksLock)
+                runningTasks.Remove(t);
+        }, TaskContinuationOptions.ExecuteSynchronously);
+    }
+
+    private void WaitForRunningTasks() {
+        Task[] tasks;
+        lock (runningTasksLock)
+            tasks = runningTasks.ToArray();
+
+        if (tasks.Length != 0)
+            SafeExtensions.Invoke(() => Task.WaitAll(tasks));
     }
 }
